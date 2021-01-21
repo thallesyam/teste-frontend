d8019fe3deaa4367b8c0d8f9e7c17c18893bdebf
Thalles Ian <thallesyam@users.=.github.com>
2021-01-21 19:38:37 -0300

    feat: Setting Next Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { Container, Header } from '../styles/pages/Home'
+
 export default function Home(): JSX.Element {
-  return <h1>CoreBiz</h1>
+  return (
+    <Container>
+      <Header>
+        <h1>CoreBiz</h1>
+      </Header>
+    </Container>
+  )
 }
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+// import { FaBars } from 'react-icons/fa'
+
+export const Container = styled.section`
+  width: 100%;
+  height: 100vh;
+`
+export const Header = styled.header`
+  width: 100%;
+  padding: 2rem 1rem;
+
+  background-color: black;
+`
