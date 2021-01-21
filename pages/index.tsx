7d3f56d1929f852700147e8db299351037862645
Thalles Ian <thallesyam@users.=.github.com>
2021-01-21 19:18:17 -0300

    feat: Setting Eslint and Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 
 const Title = styled.h1`
@@ -5,6 +6,6 @@ const Title = styled.h1`
   font-size: 50px;
 `
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return <Title>CoreBiz</Title>
 }
