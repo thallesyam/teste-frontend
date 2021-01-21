648f1cc6cd5551897165f81c23bd8c340f5d9419
Thalles Ian <thallesyam@users.=.github.com>
2021-01-21 19:27:46 -0300

    feat: Settign Styled Components

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    grayLight: '#7a7a7a',
+    grayDark: '#E6E8EA',
+    alertColor: '#F8475F'
+  }
+}
+
+export default theme
